71f641e22b8d4f94bd300475bb4bf4d72800b42a
Joacim Eberlen <jeberlee@volvocars.com>
2023-03-02 17:14:24 +0100

    adds empty interfaces

--- src/lib/models/swagger-endpoint.ts
+++ src/lib/models/swagger-endpoint.ts
@@ -27,23 +27,81 @@ export interface SwaggerResponse {
 export interface SwaggerSetup {
   swagger: string;
   info: SwaggerInfo;
+  jsonSchemaDialect?: string;
+  servers?: SwaggerServer[];
+  paths?: SwaggerPaths;
+  webHooks?: SwaggerWebHook[];
+  components?: SwaggerComponents;
+  security?: SwaggerSecurity[];
+  tags?: SwaggerTag[];
+  externalDocs?: SwaggerExternalDocuments;
   basePath: string;
 }
 
 export interface SwaggerInfo {
-  version: string;
   title: string;
-  description: string;
-  contact: SwaggerContact;
-  license: SwaggerLicense;
+  summary?: string;
+  description?: string;
+  termsOfService?: string;
+  contact?: SwaggerContact;
+  license?: SwaggerLicense;
+  version: string;
 }
 
 export interface SwaggerContact {
-  name: string;
-  url: string;
-  email: string;
+  name?: string;
+  url?: string;
+  email?: string;
 }
 
 export interface SwaggerLicense {
   name: string;
+  identifier?: string;
+  url?: string;
 }
+
+export interface SwaggerServer {
+  url: string;
+  description?: string;
+  variables?: SwaggerServerVariable[];
+}
+
+export interface SwaggerServerVariable {
+  enum?: string[];
+  default: string;
+  description?: string;
+}
+
+export interface SwaggerPaths {
+  path: SwaggerPathItem;
+}
+
+export interface SwaggerWebHook {}
+
+export interface SwaggerComponents {}
+
+export interface SwaggerSchema {}
+
+export interface SwaggerParameter {}
+
+export interface SwaggerExample {}
+
+export interface SwaggerRequestBody {}
+
+export interface SwaggerHeader {}
+
+export interface SwaggerSecurityScheme {}
+
+export interface SwaggerLink {}
+
+export interface SwaggerCallback {}
+
+export interface SwaggerPathItem {}
+
+export interface SwaggerReference {}
+
+export interface SwaggerSecurity {}
+
+export interface SwaggerTag {}
+
+export interface SwaggerExternalDocuments {}
