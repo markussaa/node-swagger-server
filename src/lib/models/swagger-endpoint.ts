5c659d222a9b8fe0944b41d6d9a6fd4fb8b9d7dc
Markus Saarijärvi <msaarija@volvocars.com>
2023-03-02 09:15:29 +0100

    adds interfaces for Swagger info fields

--- src/lib/models/swagger-endpoint.ts
+++ src/lib/models/swagger-endpoint.ts
@@ -23,3 +23,29 @@ export interface SwaggerResponses {
 export interface SwaggerResponse {
   description: string;
 }
+
+export interface SwaggerSetup {
+  swagger: string;
+  info: SwaggerInfo;
+  basePath: string;
+}
+
+export interface SwaggerInfo {
+  version: string;
+  title: string;
+  description: string;
+  contact: SwaggerContact;
+  license: SwaggerLicense;
+}
+
+export interface SwaggerContact {
+  name: string;
+  url: string;
+  email: string;
+}
+
+export interface SwaggerLicense {
+  [key: string]: {
+    name: string;
+  };
+}
