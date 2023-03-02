a62320adec68d5c5bdba0f8dbdcb6ed84e078794
Markus Saarijärvi <msaarija@volvocars.com>
2023-03-02 12:09:24 +0100

    clean up for demo

--- src/lib/services/swagger-service.ts
+++ src/lib/services/swagger-service.ts
@@ -3,24 +3,13 @@ import { SwaggerEndpoint } from "../models/swagger-endpoint";
 import * as SwaggerUI from "swagger-ui-express";
 
 export class SwaggerService {
-  private static instance: SwaggerService;
-
   private static endpoints: SwaggerEndpoint[] = [];
 
-  static getInstance(): SwaggerService {
-    console.log("Instance");
-    if (!SwaggerService.instance) {
-      SwaggerService.instance = new SwaggerService();
-    }
-
-    return SwaggerService.instance;
-  }
-
   static build(swaggerSetup: SwaggerSetup): object {
-    let blueprint = { ...swaggerSetup, paths: {} };
+    let paths: any = {};
 
     SwaggerService.endpoints.forEach((endpoint: SwaggerEndpoint) => {
-      let hejsan = {
+      let newEndpoint = {
         [endpoint.method]: {
           tags: endpoint.tags,
           summary: endpoint.summary,
@@ -30,72 +19,24 @@ export class SwaggerService {
         },
       };
 
-      blueprint.paths.hasOwnProperty(endpoint.path) ? blueprint.paths
-
-      this.swagger.paths[endpoint.path] = {
-        ...(this.swagger.paths[endpoint.path]
-          ? this.swagger.paths[endpoint.path]
-          : {}),
-        hejsan,
+      paths[endpoint.path] = {
+        ...(paths[endpoint.path] ? paths[endpoint.path] : {}),
+        ...newEndpoint,
       };
-
-      
-      blueprint.paths = { ...blueprint.paths, endpoint };
-      
-      
     });
-    return blueprint;
+
+    return { ...swaggerSetup, paths };
   }
 
   static serve(app: any, swaggerSetup: SwaggerSetup) {
-    let swaggerJson = swaggerSetup;
+    app.use(
+      "/swagger",
+      SwaggerUI.serve,
+      SwaggerUI.setup(SwaggerService.build(swaggerSetup))
+    );
   }
-  /*
-  private serveSwagger(app: any, swaggerSetup: SwaggerSetup): void {
-    app.use("/swagger", SwaggerUI.serve, SwaggerUI.setup(swagger));
-  }
-*/
+
   static addEndpoint(endpoint: SwaggerEndpoint): void {
-    /* const swagger = {
-      swagger: "2.0",
-      info: {},
-      basePath: "/api/v1",
-      consumes: "application/json",
-      produces: "application/json",
-      paths: {
-        [endpoint.path]: {
-          [endpoint.method]: {
-            tags: endpoint.tags,
-            summary: endpoint.summary,
-            description: endpoint.description,
-            parameters: endpoint.parameters,
-            responses: endpoint.responses,
-          },
-        },
-      },
-    }; */
-    /*
-    let hejsan = {
-      [endpoint.method]: {
-        tags: endpoint.tags,
-        summary: endpoint.summary,
-        description: endpoint.description,
-        parameters: endpoint.parameters,
-        responses: endpoint.responses,
-      },
-    };
-
-    this.swagger.paths[endpoint.path] = {
-      ...(this.swagger.paths[endpoint.path]
-        ? this.swagger.paths[endpoint.path]
-        : {}),
-      hejsan,
-    };
-
-    this.serveSwagger();
-    */
-
     SwaggerService.endpoints.push(endpoint);
-    console.log(SwaggerService.endpoints);
   }
 }
